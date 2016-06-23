e2c30a97463b1b9803ad4571dc0a08fd0347045a
Javier Pollak <javi.po.123@gmail.com>
2016-06-23 14:15:17 -0400

    Added short and long parameter parsing

---
+++ build/CMakeFiles/systhemer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/systhemer.dir/main.c.o"
+  "systhemer.pdb"
+  "systhemer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/systhemer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

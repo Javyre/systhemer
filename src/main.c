2a0ef699e0aefb3200c7641076376a4a51d4cda3
Javier Pollak <javi.po.123@gmail.com>
2016-06-23 19:47:09 -0400

    Added short and long parameter parsing

--- src/main.c
+++ src/main.c
@@ -1,23 +1,48 @@
+// C includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <getopt.h>
 
+//Local includes
+#include "main.h"
+
+//Package defines and dev options
 #define PACKAGE "SysThemer"
 #define VERSION "0.0.1"
-
-void print_help(int);
+#define EXIT_ON_ERR false
 
 int main(int argc, char *argv[]) {
-  static struct option const long_options[] =
-  {
-    {"help", no_argument, NULL, 'h'},
-    {"version", no_argument, NULL, 'V'},
-    {"verbose", no_argument, NULL, 'v'}    
-  };
-  int opt;
+  parseArgs(argc, argv);
+  
+  
+  return 0;
+}
 
+void print_help(int exval) {
+  printf("%s,%s show working getopt example\n", PACKAGE, VERSION); 
+  printf("%s [-h] [-V]\n\n", PACKAGE);
+
+  printf("  -h              print this help and exit\n");
+  printf("  -V              print version and exit\n");
+  printf("  -v              set verbose flag\n");
+
+  if (!exval || EXIT_ON_ERR)
+    exit(exval);
+}
+
+void parseArgs(int argc, char *argv[]) {
+  static struct option const long_options[] =
+    {
+      {"help",    no_argument, NULL, 'h'},
+      {"version", no_argument, NULL, 'V'},
+      {"verbose", no_argument, NULL, 'v'}
+    };
+  int opt;
+  
+  //If arg count is 1 because the first argument in argv is the command itself
   if(argc == 1) {
     fprintf(stderr, "This program needs arguments....\n\n");
     print_help(1);
@@ -41,17 +66,4 @@ int main(int argc, char *argv[]) {
       print_help(1);
     }
   }
-  
-  return 0;
-}
-
-void print_help(int exval) {
- printf("%s,%s show working getopt example\n", PACKAGE, VERSION); 
- printf("%s [-h] [-V]\n\n", PACKAGE);
-
- printf("  -h              print this help and exit\n");
- printf("  -V              print version and exit\n");
- printf("  -v              set verbose flag\n");
-
- exit(exval);
 }

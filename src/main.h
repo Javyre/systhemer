b5e53cff788fabb2f19d7fc896e0fb55fb0f53bf
Javier Pollak <javi.po.123@gmail.com>
2016-06-23 20:00:18 -0400

    Cleaned up some more...

--- src/main.h
+++ src/main.h
@@ -1,6 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+//C includes (that we use in this header)
+#include <stdbool.h>
+
+//Package defines and dev options (constant)
+#define PACKAGE "SysThemer"
+#define VERSION "0.0.1"
+#define EXIT_ON_ERR true
+
+//Default options (dynamic)
+bool verboseMode = false;
+
+//Function declarations
 void printHelp(int);
 void parseArgs(int argc, char *argv[]);
 

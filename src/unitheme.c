3e5cbce7ea18d4a97ee785e2c4926d72ea13c590
Javier Pollak <javi.po.123@gmail.com>
2016-07-01 18:22:34 -0400

    Started writing skeleton of UniTheme parser

--- src/unitheme.c
+++ src/unitheme.c
@@ -3,8 +3,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void loadUniTheme(const char *filename) {
+  char *buff = malloc(256);
   FILE *UniThemeFile = fopen(filename, "r");
-  
+
+  if (UniThemeFile == NULL) {
+    fprintf(stderr, "Error: file %s not found!", filename);
+    return;
+  }
+
+  while (fgets(buff, sizeof(buff), UniThemeFile) != NULL) {
+    if (buff[0] == '\n' || buff[0] == '#') {
+      continue;
+    } else {
+      getFullLine(buff);
+      evalLine(buff);
+    }
+  }
+}
+
+void getFullLine(char* currentBuffer) {
+  return;
+}
+
+void evalLine(char* currentBuffer) {
+  return;
 }
--- src/unitheme.h
+++ src/unitheme.h
@@ -1,6 +1,10 @@
 #ifndef UNITHEME_H
 #define UNITHEME_H
 
-void loadUniTheme(const char* filename);
+void loadUniTheme(const char *filename);
+
+void getFullLine(char *currentBuffer);
+
+void evalLine(char *currentBuffer);
 
 #endif
